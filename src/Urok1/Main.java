de9da94499d24abac510110d96f1a630c7778c29
Neto sergey test <serez.gavriliv@mail.ru>
2025-01-19 22:45:50 +0300

    One1

--- src/Urok1/Main.java
+++ src/Urok1/Main.java
@@ -1,9 +1,31 @@
 package Urok1;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hallo \t\n Word");
+        //System.out.println("Hallo \t\n Word");
+
+        Scanner scan = new Scanner(System.in);
+
+        // тут мы получили от пользователя страку и вывели ее на экран
+        /* System.out.println("Введите ваше имя");
+        String userName = scan.nextLine();
+        System.out.println("Привет: " + userName);
+
+        // тут написан конкулятор жима лежа
+        System.out.println("Вес штанги: ");
+        float num1 = scan.nextFloat();
+        System.out.println("Количество повторений: ");
+        float num2 = scan.nextFloat();
+        float res = (num1 * num2) / 30 + num1;
+        System.out.println("максимальный жим =  " + String.format("%.2f", res)); */
+
+
+
+
+
+
     }
 
 
